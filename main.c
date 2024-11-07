137c7ee93384a40f8ca5c77e7840f8d42b06e104
Martin Hansen <mh@debian.mh>
2024-11-07 18:33:54 +0100

    Moved implementation into  source file

--- main.c
+++ main.c
@@ -1,25 +1,6 @@
-#include "ard_m328p_ports.h"
+#define HARDCODED_IMP
 #include "ard_m328p_states.h"
-#include "wait.h"
 
 int main(){
-    D_DIRS  = ACT;
-    while (1) {
-        D_PORTS = S0;
-        wait_sec(4);
-        D_PORTS = S1;
-        wait_sec(2);
-        D_PORTS = S2;
-        wait_sec(2);
-        D_PORTS = S3;
-        wait_sec(2);
-        D_PORTS = S4;
-        wait_sec(4);
-        D_PORTS = S5;
-        wait_sec(2);
-        D_PORTS = S6;
-        wait_sec(2);
-        D_PORTS = S7;
-        wait_sec(2);
-    }
+    run();
 }
